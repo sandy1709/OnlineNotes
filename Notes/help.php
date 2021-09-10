87bb8f9811dd57b1f75e8749e3e277cf51f2af10
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-09-10 22:14:40 +0530

    publish to Github

---
+++ Notes/help.php
@@ -0,0 +1,116 @@
+<?php
+session_start();
+if(!isset($_SESSION['user_id'])){
+    header("location: index.php");
+}
+include('connection.php');
+
+$user_id = $_SESSION['user_id'];
+
+//get username and email
+$sql = "SELECT * FROM users WHERE user_id='$user_id'";
+$result = mysqli_query($link, $sql);
+
+$count = mysqli_num_rows($result);
+
+if($count == 1){
+    $row = mysqli_fetch_array($result, MYSQL_ASSOC); 
+    $username = $row['username'];
+    $email = $row['email']; 
+}else{
+    echo "There was an error retrieving the username and email from the database";   
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Help</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="styling.css" rel="stylesheet">
+      <link href='https://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+      <style>
+        #container{
+            margin-top:200px;  
+            text-align: center; 
+        }
+
+        #notePad, #allNotes, #done{
+            display: none;   
+            
+        }
+
+        .buttons{
+            margin-bottom: 20px;   
+        }
+
+        textarea{
+            width: 100%;
+            max-width: 100%;
+            font-size: 16px;
+            line-height: 1.5em;
+            border-left-width: 20px;
+            border-color: #CA3DD9;
+            color: #CA3DD9;
+            background-color: #FBEFFF;
+            padding: 10px;
+              
+        }
+          
+          tr{
+             cursor: pointer;    
+          }
+      </style>
+  </head>
+  <body>
+    <!--Navigation Bar-->  
+      <nav role="navigation" class="navbar navbar-custom navbar-fixed-top">
+      
+          <div class="container-fluid">
+            
+              <div class="navbar-header">
+              
+                  <a class="navbar-brand">Online Notes</a>
+                  <button type="button" class="navbar-toggle" data-target="#navbarCollapse" data-toggle="collapse">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  
+                  </button>
+              </div>
+              <div class="navbar-collapse collapse" id="navbarCollapse">
+                  <ul class="nav navbar-nav">
+                    <li><a href="profile.php">Profile</a></li>
+                    <li class="active"><a href="#">Help</a></li>
+                    <li><a href="contactus.php">Contact Me</a></li>
+                      <li><a href="mainpageloggedin.php">My Notes</a></li>
+                  </ul>
+                  <ul class="nav navbar-nav navbar-right">
+                      <li><a href="#">Logged in as <b><?php echo $_SESSION['username']; ?></b></a></li>
+                    <li><a href="index.php?logout=1">Log out</a></li>
+                  </ul>
+              
+              </div>
+          </div>
+      
+      </nav>
+    
+<!--Container-->
+      <div class="container" id="container">
+          <div class="row">
+                        <h2>Help </h2>
+                        <p> I guess help is not neccessary here </p>
+          </div>
+      </div>
+    <!-- Footer-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+      <script src="profile.js"></script>
+  </body>
+</html>
